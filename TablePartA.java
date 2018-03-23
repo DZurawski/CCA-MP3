ad58698555e1e26a2eb67faec6d9afb1840aabe7
DZurawski <danrzurawski@gmail.com>
2018-03-22 20:13:00 -0500

    asdf

--- TablePartA.java
+++ TablePartA.java
@@ -13,45 +13,20 @@ import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class TablePartA {
-    public static void main(String[] args) throws IOException {
-
-        // Instantiating configuration class
-        Configuration con = HBaseConfiguration.create();
-        con.clear();
-        con.set("hbase.zookeeper.quorum", "192.168.15.20");
-        con.set("hbase.zookeeper.property.clientPort", "2181");
-        
-        // Instantiating HbaseAdmin class
-        HBaseAdmin admin = new HBaseAdmin(con);
-
-        // Instantiating table descriptor class
-        HTableDescriptor tableDescriptor = new
-        HTableDescriptor(TableName.valueOf("emp"));
-
-        // Adding column families to table descriptor
-        tableDescriptor.addFamily(new HColumnDescriptor("personal"));
-        tableDescriptor.addFamily(new HColumnDescriptor("professional"));
-
-        // Execute the table through admin
-        admin.createTable(tableDescriptor);
-        System.out.println(" Table created ");
-        
-        
-/*         Configuration con = HBaseConfiguration.create();
-        HBaseAdmin admin = new HBaseAdmin(con);
-
-        HTableDescriptor powers
-            = new HTableDescriptor(TableName.valueOf("emp"));
+    public static void main(String[] args) throws IOException {        
+        HTableDescriptor powers = new HTableDescriptor("powers"); 
         powers.addFamily(new HColumnDescriptor("personal"));
         powers.addFamily(new HColumnDescriptor("professional"));
         powers.addFamily(new HColumnDescriptor("custom"));
-        admin.createTable(powers);
-        
-        HTableDescriptor food
-            = new HTableDescriptor(TableName.valueOf("emp"));
+
+        HTableDescriptor food = new HTableDescriptor("food"); 
         food.addFamily(new HColumnDescriptor("nutrition"));
         food.addFamily(new HColumnDescriptor("taste"));
-        admin.createTable(food); */
+        
+        HBaseConfiguration conf = new HBaseConfiguration(new Configuration());
+        HBaseAdmin admin = new HBaseAdmin(conf);
+        admin.createTable(powers);
+        admin.createTable(food);
     }
 }
 
