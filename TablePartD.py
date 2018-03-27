66e96e54f7886dae2c4b0ca04680e7b854b9e066
DZurawski <danrzurawski@gmail.com>
2018-03-27 15:31:46 -0500

    asdf

--- TablePartD.py
+++ TablePartD.py
@@ -8,7 +8,7 @@ row1 = powers.row("row1")
 row19 = powers.row("row19")
 
 print("hero: {0}, power: {1}, name: {2}, xp: {3}, color: {4}".format(*row1[1:]))
-print("hero: {0}, color: {1}".format(row19[1], row19[-1])
+print("hero: {0}, color: {1}".format(row19[1], row19[-1]))
 print("hero: {0}, name: {1}, color: {2}".format(row1[1], row1[3], row1[-1]))
 
 connection.close()
