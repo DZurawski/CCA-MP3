e0b58b55be4e2a51ec8b3b2b8d9c5d08a5b5aa70
DZurawski <danrzurawski@gmail.com>
2018-03-27 15:30:42 -0500

    asdf

--- TablePartD.py
+++ TablePartD.py
@@ -1,2 +1,14 @@
 import happybase as hb
 
+connection = hb.Connection("localhost", 9090)
+
+powers = connection.table("powers")
+
+row1 = powers.row("row1")
+row19 = powers.row("row19")
+
+print("hero: {0}, power: {1}, name: {2}, xp: {3}, color: {4}".format(*row1[1:]))
+print("hero: {0}, color: {1}".format(row19[1], row19[-1])
+print("hero: {0}, name: {1}, color: {2}".format(row1[1], row1[3], row1[-1]))
+
+connection.close()
